e98d17bc14f8de957938afcae65c1af3f2a8b62b
André Miranda <affmde@hotmail.com>
2022-01-30 17:14:26 +0200

    commit

--- index.js
+++ index.js
@@ -43,11 +43,9 @@ app.post("/createHighscore", async (req, res)=>{
 
 const PORT = process.env.PORT || 3001;
 
-
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(PORT, ()=>{
